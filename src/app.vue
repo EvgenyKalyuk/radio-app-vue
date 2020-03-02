079e58c50558cfd6f63e980518ab727abc86aaca
Evgeny Kalyuk <kalyuk.evgeny@gmail.com>
2020-03-03 06:34:18 +0700

    add header

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,19 @@
+<script>
+    import Header from './components/header/index.vue';
+
+    export default {
+        components: {
+            Header
+        },
+        created() {
+            console.log('created');
+        }
+    }
+</script>
+
 <template>
     <div>
+        <Header />
         hello world
         <RouterView :key="$router.fullPath" />
     </div>
---
+++ src/components/header/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <header>
+        <router-link to="/">main</router-link>
+        <router-link to="/articles">articles</router-link>
+    </header>
+</template>
